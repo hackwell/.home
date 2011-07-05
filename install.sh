77cf70c77751ab491b78d6c2679b12eb4d6fcbd3
Dominik Liebler <dominik.liebler@mayflower.de>
2011-07-05 08:37:46 +0200

    extendend install script

--- install.sh
+++ install.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 
 WD=`pwd`
+REPO="git://github.com/robbyrussell/oh-my-zsh.git"
+LOCAL_SETTINGS="$HOME/.local.zshrc"
+SETTINGS="$HOME/.zshrc"
+OK_FLAG=" [OK]"
+ZSH_PATH=`which zsh`
 
 # eventually check out oh-my-zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-	git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+	echo -n "need to clone oh-my-zsh repository from $REPO ... "
+	git clone $REPO $HOME/.oh-my-zsh
+	echo $OK_FLAG
 fi
 
 # delete existing .zshrc
-rm -f $HOME/.zshrc
+if [ -f "$SETTINGS" ]; then
+	echo -n "deleting $SETTINGS ... "
+	rm $SETTINGS
+	echo $OK_FLAG
+fi
 
 # setting new symlink
+echo -n "setting new symlink for .zshrc ... "
 ln -s $WD/.zshrc $HOME/.zshrc
+echo $OK_FLAG
 
 # check for dependencies ...
 
+# add an empty $HOME/.local.zshrc file
+if [ ! -f "$LOCAL_SETTINGS" ]; then
+	echo -n "creating $HOME/.local.zshrc file for your local settings ... "
+	touch $HOME/.local.zshrc
+	echo $OK_FLAG
+fi
+
+echo ""
+echo " => now run 'chsh -s $ZSH_PATH' to change your used shell to activate settings"
